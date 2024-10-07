a17d1fd7bc1eb535cb93744ed08fb7caa2a4226d
Pooja Kumbhar <poojakumbhar469@gmail.com>
2024-10-07 21:15:22 +0530

    Books.cs file created for Books table/information

---
+++ Library Management System/Models/Books.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library_Management_System.Models
+{
+    public class Books
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Generation { get; set; }
+        public string ISBN { get; set; }
+        public int PublicationYear { get; set; }
+        public string Status { get; set; }
+    }
+}
