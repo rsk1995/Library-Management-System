c1f7e4d000c77cabf202d7edf7397811e464c33f
SachinBansode <sachinbansode2445@gmail.com>
2024-10-07 19:09:12 +0530

    LMSDbContext.cs Class File Created for Migration.

---
+++ Library Management System/Models/LMSDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Library_Management_System.Models
+{
+    public class LMSDbContext : DbContext
+    {
+        public LMSDbContext(DbContextOptions<LMSDbContext> Options) : base(Options)
+        {
+
+        }
+           public DbSet<Users> Users {  get; set; }
+        
+    }
+}
