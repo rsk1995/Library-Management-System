11a39d617b6493a208e79823aa94756b496b0453
Pooja Kumbhar <poojakumbhar469@gmail.com>
2024-10-07 21:19:13 +0530

    Dbset added for  book table in LMSDbContext.cs file.

--- Library Management System/Models/LMSDbContext.cs
+++ Library Management System/Models/LMSDbContext.cs
@@ -10,6 +10,8 @@ namespace Library_Management_System.Models
         }
 
         public DbSet<Users> Users { get; set; }
+        public DbSet<Books> Books { get; set; }
+
         public DbSet<Transactions> Transactions { get; set; }
 
     }
