271723714b9778532bbc3b176218b11c0cbcb164
SachinBansode <sachinbansode2445@gmail.com>
2024-10-07 19:27:14 +0530

    User.cs file Created for User table / Information

---
+++ Library Management System/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Library_Management_System.Models
+{
+    public class Users
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role{get;set;}
+        public DateTime MembershipDate { get; set; } = DateTime.Now;
+        public int IsActive { get; set; } = 1;
+
+
+    }
+}
