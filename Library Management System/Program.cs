57864d490f5721b4eb4475b533cd06aa06284dba
SachinBansode <sachinbansode2445@gmail.com>
2024-10-07 19:24:08 +0530

    Database Connection string registered in Program.cs File

--- Library Management System/Program.cs
+++ Library Management System/Program.cs
@@ -1,3 +1,6 @@
+using Library_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<LMSDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
 
 var app = builder.Build();
 
