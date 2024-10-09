2839d1cb77542281c247ffd11f92cbe5d70ee6a2
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-10-09 16:09:29 +0530

    Build transaction controller for transactions like borrow, return and reserve book

--- Library Management System/Models/Transactions.cs
+++ Library Management System/Models/Transactions.cs
@@ -3,10 +3,9 @@
     public class Transactions
     {
         public int TransactionsID {get; set;}
-
         public int UserId { get; set; }
         public int BookId { get; set; }
-        public  DateTime BorrowDate { get; set; } = DateTime.Now;
+        public  DateTime BorrowDate { get; set; }
         public DateTime ReturnDate { get; set; }
         public int FineAmount { get; set; }
         
