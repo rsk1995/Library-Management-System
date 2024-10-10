e0c3984bad4f7b656ca426f7369bb419c9f02903
SachinBansode <sachinbansode2445@gmail.com>
2024-10-10 23:14:12 +0530

    Role Property Removed

---
+++ Library Management System/Models/LoginDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Library_Management_System.Models
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        //public string Role { get; set; }
+    }
+}
