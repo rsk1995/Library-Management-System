ff6086b7ade004d7f4074b47597c35fdeb7a5cbc
SachinBansode <sachinbansode2445@gmail.com>
2024-10-10 23:16:38 +0530

    JWT Token Registered in Program.cs

--- Library Management System/Program.cs
+++ Library Management System/Program.cs
@@ -1,5 +1,8 @@
+using System.Text;
 using Library_Management_System.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +14,33 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+
 builder.Services.AddDbContext<LMSDbContext>(option =>
 option.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+
+/*builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin-Only", policy => policy.RequireRole("Admin"));
+});*/
 
 var app = builder.Build();
 
@@ -22,6 +50,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+
+app.UseAuthentication();
+
 
 app.UseAuthorization();
 
