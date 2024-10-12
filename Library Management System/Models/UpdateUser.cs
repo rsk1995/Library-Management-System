c9376f0489f386613a2246cdec105774c98445fa
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-10-12 19:29:05 +0530

    UpdateUser class prepared for update user end point

---
+++ Library Management System/Models/UpdateUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Library_Management_System.Models
+{
+    public class UpdateUser
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+    }
+}
