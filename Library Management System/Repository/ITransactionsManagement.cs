6f50cfb4161027a4ea73d93fdc6a5c62f25abcd2
SachinBansode <sachinbansode2445@gmail.com>
2024-10-12 17:09:05 +0530

    Return Book Method Added

--- Library Management System/Repository/ITransactionsManagement.cs
+++ Library Management System/Repository/ITransactionsManagement.cs
@@ -6,5 +6,7 @@ namespace Library_Management_System.Repository
     public interface ITransactionsManagement
     {
         Task<Books> BorrowBook(Users user, Books book);
+        Task<Transactions> ReturnBook(Transactions extran, Books exbook);
+         Task<Transactions> GetBookById(int id);
     }
 }
