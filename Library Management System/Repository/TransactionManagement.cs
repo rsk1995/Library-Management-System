0e33c2e9746a8e6b1f2a867dc5f1afc56b70b557
SachinBansode <sachinbansode2445@gmail.com>
2024-10-12 11:59:21 +0530

    Buisness Logic Build For Transaction Management

---
+++ Library Management System/Repository/TransactionManagement.cs
@@ -0,0 +1,29 @@
+﻿using Library_Management_System.Migrations;
+using System.Net;
+using Library_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_System.Repository
+{
+    public class TransactionManagement:ITransactionsManagement
+    {
+        private readonly LMSDbContext _context;
+        public TransactionManagement(LMSDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Books> BorrowBook(Users user, Books book)
+        {
+           await _context.Transactions.AddAsync(new Transactions
+            {
+                UserId = user.UserId,
+                BookId = book.BookId,
+                BorrowDate = DateTime.Now
+            });
+            book.Status = "Checked Out";
+           await _context.SaveChangesAsync();
+            return book;
+        }
+        
+    }
+}
