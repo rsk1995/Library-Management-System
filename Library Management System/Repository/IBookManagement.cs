990c9b3fded27f30cc7ed60b08e6b6858f7db02e
SachinBansode <sachinbansode2445@gmail.com>
2024-10-12 11:44:24 +0530

    Book Interface Build for Repository Pattern

---
+++ Library Management System/Repository/IBookManagement.cs
@@ -0,0 +1,25 @@
+﻿using Library_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_System.Repository
+{
+    public interface IBookManagement
+    {
+        Task<BookDTO> AddBook(BookDTO book);
+        Task<IEnumerable<Books>> GetAllBooks();
+        Task<Books> GetBookById(int id);
+        Task<IEnumerable<Books>> GetBookByTitle(string title);
+
+        Task<IEnumerable<Books>> GetBookByAuthor(string author);
+
+        Task<IEnumerable<Books>> GetBookByPublicationYear(int year);
+
+        Task<IEnumerable<Books>> GetBookByGenration(int gen);
+        Task<IEnumerable<Books>> GetAvailableBooks();
+        Task<IEnumerable<Books>> GetCheckedOutBooks();
+        Task<IEnumerable<Books>> GetReservedBook();
+
+
+
+    }
+}
