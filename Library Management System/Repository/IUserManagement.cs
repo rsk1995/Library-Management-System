8cbc553fac04db1990b84a4d89d05de491f75491
SachinBansode <sachinbansode2445@gmail.com>
2024-10-12 11:50:37 +0530

    TransactionManagement Interface Build for Repository Pattern

---
+++ Library Management System/Repository/IUserManagement.cs
@@ -0,0 +1,14 @@
+﻿using Library_Management_System.Models;
+
+namespace Library_Management_System.Repository
+{
+    public interface IUserManagement
+    {
+        Task<UserDTO> AddUser(UserDTO user);
+        Task<IEnumerable<Users>> GetAllUsers();
+        Task<IEnumerable<Users>> GetUserByRole(string role);
+        Task<Users> GetUserById(int id);
+        Task<Users> DeactivateUser(int id);
+        Task<Users> ReactivateUser(int id);
+    }
+}
