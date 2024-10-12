785b9d8c62f6f037104b230a254bb2767229760b
SachinBansode <sachinbansode2445@gmail.com>
2024-10-12 11:57:25 +0530

    TransactionManagement Inerface Build For Repository Pattaern

---
+++ Library Management System/Repository/ITransactionsManagement.cs
@@ -0,0 +1,10 @@
+﻿using Library_Management_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_System.Repository
+{
+    public interface ITransactionsManagement
+    {
+        Task<Books> BorrowBook(Users user, Books book);
+    }
+}
