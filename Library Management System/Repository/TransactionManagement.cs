8245e72d192724a93d37803c0270304c994ed543
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-10-13 21:32:42 +0530

    Build ReserveBook & BorrowBook methods

--- Library Management System/Repository/TransactionManagement.cs
+++ Library Management System/Repository/TransactionManagement.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using Library_Management_System.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library_Management_System.Repository
 {
@@ -15,14 +16,14 @@ namespace Library_Management_System.Repository
         public async Task<Books> BorrowBook(Users user, Books book)
         {
            await _context.Transactions.AddAsync(new Transactions
-            {
+           {
                 UserId = user.UserId,
                 BookId = book.BookId,
                 BorrowDate = DateTime.Now
-            });
-            book.Status = "Checked Out";
+           });
+           book.Status = "Checked Out";
            await _context.SaveChangesAsync();
-            return book;
+           return book;
         }
 
         public async Task<Transactions> ReturnBook(Transactions extran, Books exbook)
@@ -52,6 +53,30 @@ namespace Library_Management_System.Repository
         public async Task<Transactions> GetTransactionById(int id)
         {
             return await _context.Transactions.FindAsync(id);
+        }
+
+        public async Task ReserveBook(Users user, Books book)
+        {
+            await _context.Transactions.AddAsync(new Transactions
+            {
+                UserId = user.UserId,
+                BookId = book.BookId
+            });
+            book.Status = "Reserved";
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task BorrowReserveBook(Transactions transactions, Books book)
+        {
+            book.Status = "Checked Out";
+            transactions.BorrowDate = DateTime.Now;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Transactions>> GetReservedTransaction(int uid)
+        {
+            var extrans = await _context.Transactions.Where(p => p.UserId == uid).ToListAsync();
+            return extrans;
         }
     }
 }
