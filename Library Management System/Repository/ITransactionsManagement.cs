b4562d50a79f70a6ce961c2a1a1c2f80be975d0f
RAMESH KOSHTI <koshtiramesh10@gmail.com>
2024-10-13 21:30:57 +0530

    ReserveBook & BorrowReserveBook methods declared

--- Library Management System/Repository/ITransactionsManagement.cs
+++ Library Management System/Repository/ITransactionsManagement.cs
@@ -1,5 +1,6 @@
 ﻿using Library_Management_System.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
 
 namespace Library_Management_System.Repository
 {
@@ -8,5 +9,8 @@ namespace Library_Management_System.Repository
         Task<Books> BorrowBook(Users user, Books book);
         Task<Transactions> ReturnBook(Transactions extran, Books exbook);
         Task<Transactions> GetTransactionById(int id);
+        Task ReserveBook(Users user, Books book);
+        Task BorrowReserveBook(Transactions transactions, Books book);
+        Task<IEnumerable<Transactions>> GetReservedTransaction(int uid);
     }
 }
